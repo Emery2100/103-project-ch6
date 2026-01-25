5e42422d5a0b2030311feed358d8ffab9af825e6
David Emery <Emery2100@yahoo.com>
2026-01-24 20:03:13 -0800

    Initial Commit

---
+++ 103-project-ch6/_03_project_ch6App.swift
@@ -0,0 +1,17 @@
+//
+//  _03_project_ch6App.swift
+//  103-project-ch6
+//
+//  Created by David Emery on 1/24/26.
+//
+
+import SwiftUI
+
+@main
+struct _03_project_ch6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
